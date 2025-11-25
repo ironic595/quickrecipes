d4af50c9ddb52458c40188552fa34aa5903f023b
ironic595 <ironic595@gmail.com>
2025-11-24 21:48:13 -0300

    Create dataload.js

---
+++ dataload.js
@@ -0,0 +1,5 @@
+fetch('https://script.google.com/macros/s/AKfycby6cjKcusU4cWQCxpf3-IBVSDSRb9qwostLxtXzlUHyMdAElt0-Acha28JJjQNbLQ5V/exec')
+     .then(response => response.json())
+     .then(data => {
+       // Filtrar y crear divs como mencionaste antes
+     });
